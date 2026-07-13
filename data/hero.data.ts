9d2f2cfc233621c05c60e377c93232d1a4341a05
Plagiaryze2 <muhammadanas1243@gmail.com>
2026-07-13 18:10:00 +0500

    Fixed image routes, paths, renamed images.

--- data/hero.data.ts
+++ data/hero.data.ts
@@ -1,7 +1,7 @@
 import type { HeroSlide } from "@/types";
 
 export const heroSlidesData: HeroSlide[] = [
-  { id: "slide1", image: "/images/hero/slide-1.jpg", alt: "Cinqo Holdings project 1" },
-  { id: "slide2", image: "/images/hero/slide-2.jpg", alt: "Cinqo Holdings project 2" },
-  { id: "slide3", image: "/images/hero/slide-3.jpg", alt: "Cinqo Holdings project 3" },
+  { id: "slide1", image: "/images/hero/h1.jpg", alt: "Cinqo Holdings project 1" },
+  { id: "slide2", image: "/images/hero/h2.jpg", alt: "Cinqo Holdings project 2" },
+  { id: "slide3", image: "/images/hero/h3.jpg", alt: "Cinqo Holdings project 3" },
 ];
