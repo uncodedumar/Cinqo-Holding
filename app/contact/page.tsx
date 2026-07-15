3a18a5f6f42ed5121246e02d157a8892a480d977
Muhammad Hasan Butt <hasan.butt2005@gmail.com>
2026-07-15 12:56:23 +0500

    Rename Contact and GetInTouch

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/sections/Contact/Hero";
-import ContactUs from "@/components/sections/Contact/ContactUs";
-import GetInTouch from "@/components/sections/Contact/GetInTouch";
+import ContactUs from "@/components/sections/Contact/GetInTouch";
+import GetInTouch from "@/components/sections/Contact/ContactUs";
 import Location from "@/components/sections/Contact/Location";
 import News from "@/components/sections/Contact/News";
 
