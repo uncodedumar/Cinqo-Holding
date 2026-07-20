0ae4710aa87aa6b1e43f7d85e55c40b533f45ce4
Muhammad Hasan Butt <hasan.butt2005@gmail.com>
2026-07-20 17:56:08 +0500

    Fixed the image Name

--- components/sections/About/Team.tsx
+++ components/sections/About/Team.tsx
@@ -24,7 +24,7 @@ const executives = [
   { name: "Ranjith", designation: "General Manager – Cinqo Trading", image: "/images/about/Ranjith.png" },
   { name: "Mohd Ali", designation: "General Manager – THC Facilities Management", image: "/images/about/Ali.png" },
   { name: "Collins", designation: "General Manager – THC Fit Out", image: "/images/about/Collins.png" },
-  { name: "Prakash_", designation: "Group Finance Manager", image: "/images/about/Prakash.png" },
+  { name: "Prakash", designation: "Group Finance Manager", image: "/images/about/Prakash_.png" },
   { name: "Satish", designation: "Group HR Manager", image: "/images/about/Satish.png" },
   { name: "Alok", designation: "Group Business Development Manager", image: "/images/about/Alok.png" },
   { name: "Selina", designation: "Group Admin Manager", image: "/images/about/Selina.png" },
