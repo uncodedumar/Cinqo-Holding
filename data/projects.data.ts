3d9a06c6b9f877682f316f595df8f79dcf02206e
ErenYeagerJinko <erenyeagerabdulahad@gmail.com>
2026-07-31 00:43:48 +0500

    Added all Projs

--- data/projects.data.ts
+++ data/projects.data.ts
@@ -114,4 +114,439 @@ export const PROJECTS: Project[] = [
       "/images/projects/fitout/Maserati/4.jpeg"
     ]
   },
+  {
+    id: "j009",
+    name: "3 Storey Building at Janabiyah",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/3 Storey Building at Janabiyah/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for 3 Storey Building at Janabiyah.",
+    thumbnails: [
+      "/images/projects/flooring/3 Storey Building at Janabiyah/2.jpg",
+      "/images/projects/flooring/3 Storey Building at Janabiyah/3.jpg",
+      "/images/projects/flooring/3 Storey Building at Janabiyah/4.jpg"
+    ]
+  },
+  {
+    id: "j010",
+    name: "Adliya 7 Storey Project",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/Adliya 7 Storey Project/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for Adliya 7 Storey Project.",
+    thumbnails: [
+      "/images/projects/flooring/Adliya 7 Storey Project/2.jpg",
+      "/images/projects/flooring/Adliya 7 Storey Project/3.jpg",
+      "/images/projects/flooring/Adliya 7 Storey Project/4.jpg"
+    ]
+  },
+  {
+    id: "j011",
+    name: "AL WASEET PLASTIC COMPANY FACTORY, KUWAIT",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/AL WASEET PLASTIC COMPANY FACTORY, KUWAIT/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for AL WASEET PLASTIC COMPANY FACTORY, KUWAIT.",
+    thumbnails: [
+      "/images/projects/flooring/AL WASEET PLASTIC COMPANY FACTORY, KUWAIT/2.jpg",
+      "/images/projects/flooring/AL WASEET PLASTIC COMPANY FACTORY, KUWAIT/3.jpg",
+      "/images/projects/flooring/AL WASEET PLASTIC COMPANY FACTORY, KUWAIT/4.jpg"
+    ]
+  },
+  {
+    id: "j012",
+    name: "AMAS JOTUN",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/AMAS JOTUN/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for AMAS JOTUN.",
+    thumbnails: [
+      "/images/projects/flooring/AMAS JOTUN/2.jpg",
+      "/images/projects/flooring/AMAS JOTUN/3.jpg",
+      "/images/projects/flooring/AMAS JOTUN/4.jpg"
+    ]
+  },
+  {
+    id: "j013",
+    name: "AVENUES MALL - BERGER",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/AVENUES MALL - BERGER/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for AVENUES MALL - BERGER.",
+    thumbnails: [
+      "/images/projects/flooring/AVENUES MALL - BERGER/2.jpg",
+      "/images/projects/flooring/AVENUES MALL - BERGER/3.jpg",
+      "/images/projects/flooring/AVENUES MALL - BERGER/4.jpg"
+    ]
+  },
+  {
+    id: "j014",
+    name: "AVS Villa",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/AVS�Villa/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for AVS Villa.",
+    thumbnails: [
+      "/images/projects/flooring/AVS�Villa/2.jpg",
+      "/images/projects/flooring/AVS�Villa/3.jpg",
+      "/images/projects/flooring/AVS�Villa/4.jpg"
+    ]
+  },
+  {
+    id: "j015",
+    name: "Bader Noorudin Villa",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/Bader Noorudin�Villa/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for Bader Noorudin Villa.",
+    thumbnails: [
+      "/images/projects/flooring/Bader Noorudin�Villa/2.jpg",
+      "/images/projects/flooring/Bader Noorudin�Villa/3.jpg",
+      "/images/projects/flooring/Bader Noorudin�Villa/4.jpg"
+    ]
+  },
+  {
+    id: "j016",
+    name: "BAHRAIN AIRPORT",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/BAHRAIN AIRPORT/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for BAHRAIN AIRPORT.",
+    thumbnails: [
+      "/images/projects/flooring/BAHRAIN AIRPORT/2.jpg",
+      "/images/projects/flooring/BAHRAIN AIRPORT/3.jpg",
+      "/images/projects/flooring/BAHRAIN AIRPORT/4.jpg"
+    ]
+  },
+  {
+    id: "j017",
+    name: "CITY CENTRE BAHRAIN",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/CITY CENTRE BAHRAIN/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for CITY CENTRE BAHRAIN.",
+    thumbnails: [
+      "/images/projects/flooring/CITY CENTRE BAHRAIN/2.jpg",
+      "/images/projects/flooring/CITY CENTRE BAHRAIN/3.jpg"
+    ]
+  },
+  {
+    id: "j018",
+    name: "EXHIBITION WORLD",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/EXHIBITION WORLD/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for EXHIBITION WORLD.",
+    thumbnails: [
+      "/images/projects/flooring/EXHIBITION WORLD/2.jpg",
+      "/images/projects/flooring/EXHIBITION WORLD/3.jpg",
+      "/images/projects/flooring/EXHIBITION WORLD/4.jpg"
+    ]
+  },
+  {
+    id: "j019",
+    name: "FGR PHASE 2",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/FGR PHASE 2/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for FGR PHASE 2."
+  },
+  {
+    id: "j020",
+    name: "FONTANA INFINITY",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/FONTANA INFINITY/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for FONTANA INFINITY.",
+    thumbnails: [
+      "/images/projects/flooring/FONTANA INFINITY/2.jpg",
+      "/images/projects/flooring/FONTANA INFINITY/3.jpeg",
+      "/images/projects/flooring/FONTANA INFINITY/4.png"
+    ]
+  },
+  {
+    id: "j021",
+    name: "Hamala Villa",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/Hamala Villa/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for Hamala Villa.",
+    thumbnails: [
+      "/images/projects/flooring/Hamala Villa/2.jpg",
+      "/images/projects/flooring/Hamala Villa/3.jpg",
+      "/images/projects/flooring/Hamala Villa/4.jpg"
+    ]
+  },
+  {
+    id: "j022",
+    name: "HAWAR",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/HAWAR/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for HAWAR.",
+    thumbnails: [
+      "/images/projects/flooring/HAWAR/2.jpg",
+      "/images/projects/flooring/HAWAR/3.jpg",
+      "/images/projects/flooring/HAWAR/4.jpg"
+    ]
+  },
+  {
+    id: "j023",
+    name: "HIDD OASIS",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/HIDD OASIS/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for HIDD OASIS.",
+    thumbnails: [
+      "/images/projects/flooring/HIDD OASIS/2.jpeg",
+      "/images/projects/flooring/HIDD OASIS/3.jpeg",
+      "/images/projects/flooring/HIDD OASIS/4.jpeg"
+    ]
+  },
+  {
+    id: "j024",
+    name: "IKEA",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/IKEA/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for IKEA.",
+    thumbnails: [
+      "/images/projects/flooring/IKEA/2.jpg",
+      "/images/projects/flooring/IKEA/3.jpg",
+      "/images/projects/flooring/IKEA/4.jpg"
+    ]
+  },
+  {
+    id: "j025",
+    name: "JUFFAIR SQUARE (JRD) BERGER",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/JUFFAIR SQUARE (JRD) BERGER/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for JUFFAIR SQUARE (JRD) BERGER.",
+    thumbnails: [
+      "/images/projects/flooring/JUFFAIR SQUARE (JRD) BERGER/2.jpg",
+      "/images/projects/flooring/JUFFAIR SQUARE (JRD) BERGER/3.jpg",
+      "/images/projects/flooring/JUFFAIR SQUARE (JRD) BERGER/4.jpg"
+    ]
+  },
+  {
+    id: "j026",
+    name: "KING HAMAD BERGER",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/KING HAMAD BERGER/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for KING HAMAD BERGER.",
+    thumbnails: [
+      "/images/projects/flooring/KING HAMAD BERGER/2.jpg",
+      "/images/projects/flooring/KING HAMAD BERGER/3.jpg",
+      "/images/projects/flooring/KING HAMAD BERGER/4.jpg"
+    ]
+  },
+  {
+    id: "j027",
+    name: "LAGOON VIEWS",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/LAGOON VIEWS/1.png",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for LAGOON VIEWS.",
+    thumbnails: [
+      "/images/projects/flooring/LAGOON VIEWS/2.png",
+      "/images/projects/flooring/LAGOON VIEWS/3.png",
+      "/images/projects/flooring/LAGOON VIEWS/4.jpg"
+    ]
+  },
+  {
+    id: "j028",
+    name: "MARASSI",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/MARASSI/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for MARASSI."
+  },
+  {
+    id: "j029",
+    name: "MARINA",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/MARINA/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for MARINA.",
+    thumbnails: [
+      "/images/projects/flooring/MARINA/2.jpg",
+      "/images/projects/flooring/MARINA/3.jpg",
+      "/images/projects/flooring/MARINA/4.jpg"
+    ]
+  },
+  {
+    id: "j030",
+    name: "MKF -JOTUN",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/MKF -JOTUN/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for MKF -JOTUN.",
+    thumbnails: [
+      "/images/projects/flooring/MKF -JOTUN/2.jpg",
+      "/images/projects/flooring/MKF -JOTUN/3.jpg",
+      "/images/projects/flooring/MKF -JOTUN/4.jpg"
+    ]
+  },
+  {
+    id: "j031",
+    name: "OASIS MALL BERGER",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/OASIS MALL BERGER/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for OASIS MALL BERGER.",
+    thumbnails: [
+      "/images/projects/flooring/OASIS MALL BERGER/2.jpg",
+      "/images/projects/flooring/OASIS MALL BERGER/3.jpg",
+      "/images/projects/flooring/OASIS MALL BERGER/4.jpg"
+    ]
+  },
+  {
+    id: "j032",
+    name: "ONYX TOWER",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/ONYX TOWER/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for ONYX TOWER.",
+    thumbnails: [
+      "/images/projects/flooring/ONYX TOWER/2.jpg",
+      "/images/projects/flooring/ONYX TOWER/3.jpg",
+      "/images/projects/flooring/ONYX TOWER/4.jpg"
+    ]
+  },
+  {
+    id: "j033",
+    name: "RES AT BARBAR",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/RES AT BARBAR/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for RES AT BARBAR.",
+    thumbnails: [
+      "/images/projects/flooring/RES AT BARBAR/2.jpg",
+      "/images/projects/flooring/RES AT BARBAR/3.jpg"
+    ]
+  },
+  {
+    id: "j034",
+    name: "SA2",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/SA2/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for SA2.",
+    thumbnails: [
+      "/images/projects/flooring/SA2/2.jpg",
+      "/images/projects/flooring/SA2/3.jpg",
+      "/images/projects/flooring/SA2/4.jpg"
+    ]
+  },
+  {
+    id: "j035",
+    name: "SBV - JOTUN",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/SBV - JOTUN/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for SBV - JOTUN.",
+    thumbnails: [
+      "/images/projects/flooring/SBV - JOTUN/2.jpg",
+      "/images/projects/flooring/SBV - JOTUN/3.jpg",
+      "/images/projects/flooring/SBV - JOTUN/4.jpg"
+    ]
+  },
+  {
+    id: "j036",
+    name: "STP TUBLI",
+    subheading: "Dummy subheading",
+    date: "1 Jan, 2026",
+    status: "completed",
+    image: "/images/projects/flooring/STP TUBLI/1.jpg",
+    logo: "/images/logos/4.png",
+    company: "Cinqo Holding",
+    description: "Dummy description for STP TUBLI.",
+    thumbnails: [
+      "/images/projects/flooring/STP TUBLI/2.jpg",
+      "/images/projects/flooring/STP TUBLI/3.jpg"
+    ]
+  }
 ];
